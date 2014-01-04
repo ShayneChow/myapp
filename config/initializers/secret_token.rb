4f56cd1bda7aaa54f27c9c03a3bc06fcb3792f3d
Shayne <shayne@authbuy.org>
2014-01-04 11:29:45 +0800

    初始化git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Myapp::Application.config.secret_key_base = '990700d02fe6f1240cb97fa8aebab2fae0f040cd96f50836e72ebf83729e8b4c19f681a3c059a6ba9d75d371110a00d43cb6c3c1465ab082abd8040f211113ea'
