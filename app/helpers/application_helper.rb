60d24028a6050bd33aeea0d6d8c1d1a35f02d22d
Shayne <shayne@authbuy.org>
2014-01-06 12:32:16 +0800

    完成layout布局和路由设置

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+	#公共标题 帮助方法
+	def full_title(page_title)
+		base_title = "Ruby on Rails Tutorial Sample App" #公共部分
+		if page_title.empty?
+			base_title
+		else
+			"#{base_title} | #{page_title}"
+		end
+	end
 end
